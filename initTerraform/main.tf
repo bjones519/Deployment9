0396979ac8e64bddac7ed8cab6f840540300d33d
belindadunu <belindadunu24@gmail.com>
2023-11-16 01:56:49 +0000

    add vpc

---
+++ initTerraform/main.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+
+}
+
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "172.28.0.0/16"
+  tags = {
+    "Name" = "eks_vpc"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "172.28.0.0/18"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public | us-east-1a"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "172.28.64.0/18"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "private | us-east-1a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "172.28.128.0/18"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public | us-east-1b"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "172.28.192.0/18"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "private | us-east-1b"
+  }
+}
+
+resource "aws_route_table_association" "public_a_subnet" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_a_subnet" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_b_subnet" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_b_subnet" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "elastic-ip" {
+  domain = "vpc"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_nat_gateway" "ngw" {
+  subnet_id     = aws_subnet.public_a.id
+  allocation_id = aws_eip.elastic-ip.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private_ngw" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
+}
