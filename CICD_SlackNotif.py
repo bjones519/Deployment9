74394335b673e0d6ec9002164d0ead642dcc1b2e
Brittney jones <brittneyjones@Brittneys-MBP.lan>
2023-11-16 13:01:26 -0500

    Adding script and adding script stage to Jenkinsfile

---
+++ CICD_SlackNotif.py
@@ -0,0 +1,28 @@
+#Create a Python script that will send a Slack message to your group chat when the Jenkins pipeline is done.
+
+import requests
+import os
+import json
+
+def slack_notif(message):
+    url = os.getenv("webhook_url")
+    content={
+        "text": message
+    }
+
+    post = requests.post(url, json=content)
+
+    if post.status_code != 200:
+        print(f"Failed to send Slack message. Status code: {post.status_code}")
+    else:
+        print("Slack message sent successfully!")
+
+
+pipeline_status = os.getenv("pipeline_status")
+
+if pipeline_status == "SUCCESS":
+    message = "Jenkins pipeline completed successfully!"
+    slack_notif(message)
+else:
+    message = "Jenkins pipeline did not complete successfully!"
+    slack_notif(message)
