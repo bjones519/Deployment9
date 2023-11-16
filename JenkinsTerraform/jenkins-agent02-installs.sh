b288ef8e7d9c3c0a6d235a72478efc658b5e4350
belindadunu <belindadunu24@gmail.com>
2023-11-16 01:32:17 +0000

    download eksctl

--- JenkinsTerraform/jenkins-agent02-installs.sh
+++ JenkinsTerraform/jenkins-agent02-installs.sh
@@ -1,77 +1,30 @@
 #!/bin/bash
 
-#################### Install Java: ####################
-sudo apt-get update -y
-sudo apt-get install -y default-jre
-sudo apt-get update -y
-
-#################### Install kubectl: ####################
-
-#################### Update the apt package index and install packages needed to use the Kubernetes apt repository ####################
+exec > >(tee -a /var/log/user-data.log) 2>&1
+echo “Script started at: $(date)”
 sudo apt-get update
-# apt-transport-https may be a dummy package; if so, you can skip that package
-sudo apt-get install -y apt-transport-https ca-certificates curl
 
-#################### Download the public signing key for the Kubernetes package repositories ####################
+#install java
+sudo apt install -y fontconfig -y openjdk-17-jre
+sudo apt update
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+#download aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install -y zip
+unzip awscliv2.zip
+sudo ./aws/install
 
-#################### Add the appropriate Kubernetes apt repository ####################
-#################### This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list ####################
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#aws configure
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 
-#################### Update apt package index, then install kubectl: ####################
-sudo apt-get update
-sudo snap install kubectl
-sudo apt-get install -y kubectl
+#make executable
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
-#################### Ensure the version you installed is up-to-date: ####################
-kubectl version --client
+#kubectl version --short
 
-#################### Installation complete ####################
-echo "-----------------------kubectl successfully installed!-----------------------"
+#download eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
-
-
-
-##########################Come back to separate files##########################
-
-# #!/bin/bash
-
-# #################### Install Java ####################
-# sudo apt-get update -y
-# sudo apt install -y default-jre
-# sudo apt-get update -y
-
-# #################### Install Terraform ####################
-# sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-
-# #################### Install the HashiCorp GPG key  ####################
-# wget -O- https://apt.releases.hashicorp.com/gpg | \
-# gpg --dearmor | \
-# sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-
-# #################### Verify the key's fingerprint ####################
-# gpg --no-default-keyring \
-# --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-# --fingerprint
-
-# #################### Add the official HashiCorp repository to your system ####################
-# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-# https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-# sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# #################### Download the package information from HashiCorp ####################
-# sudo apt update
-
-# #################### Install Terraform from the new repository ####################
-# sudo apt-get install -y terraform
-
-# #################### Verify the installation ####################
-# terraform -help
-
-# #################### Done! ####################
-# echo "Installations successfully installed!"
-
-# exit 0
+#moving eks cli binary to user path
+sudo mv /tmp/eksctl /usr/local/bin
